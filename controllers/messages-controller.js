d70d14dea82af70e48015322d5cb306a2557c728
Claudia Teng <sydeng102@gmail.com>
2021-10-14 17:29:07 +0800

    sending files

--- controllers/messages-controller.js
+++ controllers/messages-controller.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 const getMessage = (req, res) => {
-  res.send('<ul><li>Hello Albert</li></ul>');
+  res.sendFile(path.join(__dirname, '..', 'public', 'skimountain.jpg'));
+  // res.send('<ul><li>Hello Albert</li></ul>');
 }
 
 module.exports = {
